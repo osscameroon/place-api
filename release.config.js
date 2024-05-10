2a5269ef75bd54d3910fae616b70bff08c86c980
GenjiruSUchiwa <genjiru23@gmail.com>
2024-05-11 00:26:30 +0200

    ci: fix semantic file

--- release.config.js
+++ release.config.js
@@ -4,10 +4,6 @@
         "@semantic-release/github",
         "@semantic-release/commit-analyzer",
         "@semantic-release/release-notes-generator",
-        ["@semantic-release/exec", {
-            "verifyConditionsCmd": "./verify.sh",
-            "publishCmd": "./publish.sh ${nextRelease.version} ${branch.name} ${commits.length} ${Date.now()}"
-        }],
         ["@semantic-release/git", {
             "message": "chore(release): ${nextRelease.version} [skip-ci] \n\n${nextRelease.notes}"
         }],
