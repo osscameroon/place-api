c1852fcc63a76ec46f2a6dae346cfd4b8c14a277
GenjiruSUchiwa <genjiru23@gmail.com>
2024-05-11 00:22:43 +0200

    ci: fix semantic file

--- release.config.js
+++ release.config.js
@@ -1,18 +1,20 @@
-﻿{
-  "branches": ["main"],
+﻿const config = {
+    "branches": ["main"],
     "plugins": [
-      "@semantic-release/github",
-      "@semantic-release/commit-analyzer",
-      "@semantic-release/release-notes-generator",
-      "@semantic-release/git":{
-      "message": "chore(release): ${nextRelease.version} [skip-ci] \n\n${nextRelease.notes}"
-      },
-    ["@semantic-release/exec", {
-        "verifyConditionsCmd": "./verify.sh",
-        "publishCmd": "./publish.sh ${nextRelease.version} ${branch.name} ${commits.length} ${Date.now()}"
-    }],
-      ["@semantic-release/changelog", {
+        "@semantic-release/github",
+        "@semantic-release/commit-analyzer",
+        "@semantic-release/release-notes-generator",
+        ["@semantic-release/exec", {
+            "verifyConditionsCmd": "./verify.sh",
+            "publishCmd": "./publish.sh ${nextRelease.version} ${branch.name} ${commits.length} ${Date.now()}"
+        }],
+        ["@semantic-release/git", {
+            "message": "chore(release): ${nextRelease.version} [skip-ci] \n\n${nextRelease.notes}"
+        }],
+        ["@semantic-release/changelog", {
             "changelogFile": "CHANGELOG.md"
-          }]
+        }]
     ]
 }
+
+module.exports = config
