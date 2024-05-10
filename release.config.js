d032fddbed61ef8122f279f1dc3abd3a6683a35d
GenjiruSUchiwa <genjiru23@gmail.com>
2024-05-11 00:16:34 +0200

    ci: semantic release

--- release.config.js
+++ release.config.js
@@ -7,7 +7,10 @@
       "@semantic-release/git":{
       "message": "chore(release): ${nextRelease.version} [skip-ci] \n\n${nextRelease.notes}"
       },
-      "@semantic-release/exec",
+    ["@semantic-release/exec", {
+        "verifyConditionsCmd": "./verify.sh",
+        "publishCmd": "./publish.sh ${nextRelease.version} ${branch.name} ${commits.length} ${Date.now()}"
+    }],
       ["@semantic-release/changelog", {
             "changelogFile": "CHANGELOG.md"
           }]
