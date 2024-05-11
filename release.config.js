4c7e91c13947b8a24bf6431dcc7c249c00afdcd1
GenjiruSUchiwa <genjiru23@gmail.com>
2024-05-11 09:42:18 +0200

    ci: update semantic release configuration file

--- release.config.js
+++ release.config.js
@@ -4,14 +4,21 @@
         "@semantic-release/github",
         "@semantic-release/commit-analyzer",
         "@semantic-release/release-notes-generator",
-        ["@semantic-release/changelog", {
-            "changelogFile": "CHANGELOG.md"
-        }]
-        ["@semantic-release/git", {
-            "message": "chore(release): ${nextRelease.version} [skip-ci] \n\n${nextRelease.notes}",
-            "assets": ["CHANGELOG.md"]
-        }]
+        [
+            "@semantic-release/changelog",
+            {
+                "changelogFile": "CHANGELOG.md"
+            }
+        ],
+        [
+            "@semantic-release/git",
+            {
+                "message": "chore(release): ${nextRelease.version} [skip-ci] \n\n${nextRelease.notes}",
+                "assets": ["CHANGELOG.md"]
+            }
+        ]
     ]
+
 }
 
 module.exports = config
