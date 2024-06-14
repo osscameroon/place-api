5264a50c8c09acf982de586dc9289da85f22f91b
GenjiruSUchiwa <genjiru23@gmail.com>
2024-06-14 20:20:29 +0200

    chore: Adding serilog

--- src/PlaceApi.Web/Program.cs
+++ src/PlaceApi.Web/Program.cs
@@ -1,11 +1,31 @@
+using System.Collections.Generic;
+using System.Reflection;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
+using PlaceApi.SharedKernel;
 using PlaceApi.Web;
+using Serilog;
+
+ILogger logger = Log.Logger = new LoggerConfiguration()
+    .Enrich.FromLogContext()
+    .WriteTo.Console()
+    .CreateLogger();
+
+logger.Information("Starting web host");
 
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 IServiceCollection services = builder.Services;
 
 services.AddWebApplication(builder.Configuration);
+
+// Add Module Services
+List<Assembly> mediatRAssemblies = [typeof(Program).Assembly];
+builder.Services.AddMediatR(cfg => cfg.RegisterServicesFromAssemblies(mediatRAssemblies.ToArray()));
+builder.Services.AddMediatRLoggingBehavior();
+builder.Services.AddMediatRFluentValidationBehavior();
+
+// Add MediatR Domain Event Dispatcher
+builder.Services.AddScoped<IDomainEventDispatcher, MediatRDomainEventDispatcher>();
 
 WebApplication app = builder.Build();
 
