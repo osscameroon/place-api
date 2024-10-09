9873e30188397f4c1653639597fdf1f03f658c45
Aristide Herve Mbassi <aristideherve.mbassidemoudourou@veepee.comm>
2024-10-09 22:37:41 +0200

    refactor: improve readability

--- src/PlaceAPi.Identity/Authenticate/Endpoints/Register.cs
+++ src/PlaceAPi.Identity/Authenticate/Endpoints/Register.cs
@@ -1,9 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
-using System.Diagnostics;
-using System.Text;
-using System.Text.Encodings.Web;
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
@@ -11,9 +9,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.Data;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Routing;
-using Microsoft.AspNetCore.WebUtilities;
 using Microsoft.Extensions.DependencyInjection;
 
 public static partial class AuthenticationEndpointsExtensions
@@ -23,120 +19,77 @@ public static partial class AuthenticationEndpointsExtensions
     public static IEndpointRouteBuilder MapRegisterEndpoint<TUser>(this IEndpointRouteBuilder group)
         where TUser : class, new()
     {
-        IEmailSender<TUser> emailSender = group.ServiceProvider.GetRequiredService<
-            IEmailSender<TUser>
-        >();
-        LinkGenerator linkGenerator = group.ServiceProvider.GetRequiredService<LinkGenerator>();
-
-        // We'll figure out a unique endpoint name based on the final route pattern during endpoint generation.
-        group.MapPost(
-            "/register",
-            async Task<Results<Ok, ValidationProblem>> (
-                [FromBody] RegisterRequest registration,
-                HttpContext context,
-                [FromServices] IServiceProvider sp
-            ) =>
-            {
-                UserManager<TUser> userManager = sp.GetRequiredService<UserManager<TUser>>();
-
-                if (!userManager.SupportsUserEmail)
-                {
-                    throw new NotSupportedException(
-                        $"{nameof(MapRegisterEndpoint)} requires a user store with email support."
-                    );
-                }
-
-                IUserStore<TUser> userStore = sp.GetRequiredService<IUserStore<TUser>>();
-                IUserEmailStore<TUser> emailStore = (IUserEmailStore<TUser>)userStore;
-                string email = registration.Email;
-
-                if (string.IsNullOrEmpty(email) || !EmailAddressAttribute.IsValid(email))
-                {
-                    return CreateValidationProblem(
-                        IdentityResult.Failed(userManager.ErrorDescriber.InvalidEmail(email))
-                    );
-                }
-
-                TUser user = new TUser();
-                await userStore.SetUserNameAsync(user, email, CancellationToken.None);
-                await emailStore.SetEmailAsync(user, email, CancellationToken.None);
-                IdentityResult result = await userManager.CreateAsync(user, registration.Password);
-
-                if (!result.Succeeded)
-                {
-                    return CreateValidationProblem(result);
-                }
-
-                await SendConfirmationEmailAsync(user, userManager, context, email);
-                return TypedResults.Ok();
-            }
-        );
+        group.MapPost("/register", RegisterHandler);
         return group;
 
-        async Task SendConfirmationEmailAsync(
-            TUser user,
-            UserManager<TUser> userManager,
+        static async Task<Results<Ok, ValidationProblem>> RegisterHandler(
+            [FromBody] RegisterRequest registration,
             HttpContext context,
-            string email,
-            bool isChange = false
+            [FromServices] IServiceProvider sp
         )
         {
-            if (confirmEmailEndpointName is null)
+            UserManager<TUser> userManager = sp.GetRequiredService<UserManager<TUser>>();
+            IEmailSender<TUser> emailSender = sp.GetRequiredService<IEmailSender<TUser>>();
+            LinkGenerator linkGenerator = sp.GetRequiredService<LinkGenerator>();
+
+            if (!userManager.SupportsUserEmail)
             {
-                throw new NotSupportedException("No email confirmation endpoint was registered!");
-            }
-
-            string code = isChange
-                ? await userManager.GenerateChangeEmailTokenAsync(user, email)
-                : await userManager.GenerateEmailConfirmationTokenAsync(user);
-            code = WebEncoders.Base64UrlEncode(Encoding.UTF8.GetBytes(code));
-
-            string userId = await userManager.GetUserIdAsync(user);
-            RouteValueDictionary routeValues = new() { ["userId"] = userId, ["code"] = code };
-
-            if (isChange)
-            {
-                routeValues.Add("changedEmail", email);
-            }
-
-            string confirmEmailUrl =
-                linkGenerator.GetUriByName(context, confirmEmailEndpointName, routeValues)
-                ?? throw new NotSupportedException(
-                    $"Could not find endpoint named '{confirmEmailEndpointName}'."
+                throw new NotSupportedException(
+                    $"{nameof(MapRegisterEndpoint)} requires a user store with email support."
                 );
+            }
 
-            await emailSender.SendConfirmationLinkAsync(
+            if (!IsValidEmail(registration.Email))
+            {
+                return CreateValidationProblem(
+                    IdentityResult.Failed(
+                        userManager.ErrorDescriber.InvalidEmail(registration.Email)
+                    )
+                );
+            }
+
+            (TUser user, IdentityResult result) = await CreateUserAsync<TUser>(sp, registration);
+
+            if (!result.Succeeded)
+            {
+                return CreateValidationProblem(result);
+            }
+
+            await SendConfirmationEmailAsync(
+                emailSender,
+                linkGenerator,
                 user,
-                email,
-                HtmlEncoder.Default.Encode(confirmEmailUrl)
+                userManager,
+                context,
+                registration.Email
             );
+            return TypedResults.Ok();
         }
+    }
+
+    private static async Task<(TUser user, IdentityResult result)> CreateUserAsync<TUser>(
+        IServiceProvider sp,
+        RegisterRequest registration
+    )
+        where TUser : class, new()
+    {
+        UserManager<TUser> userManager = sp.GetRequiredService<UserManager<TUser>>();
+        IUserStore<TUser> userStore = sp.GetRequiredService<IUserStore<TUser>>();
+        IUserEmailStore<TUser> emailStore = (IUserEmailStore<TUser>)userStore;
+
+        TUser user = new();
+        await userStore.SetUserNameAsync(user, registration.Email, CancellationToken.None);
+        await emailStore.SetEmailAsync(user, registration.Email, CancellationToken.None);
+        IdentityResult result = await userManager.CreateAsync(user, registration.Password);
+
+        return (user, result);
     }
 
     private static ValidationProblem CreateValidationProblem(IdentityResult result)
     {
-        // We expect a single error code and description in the normal case.
-        // This could be golfed with GroupBy and ToDictionary, but perf! :P
-        Debug.Assert(!result.Succeeded);
-        Dictionary<string, string[]> errorDictionary = new Dictionary<string, string[]>(1);
-
-        foreach (IdentityError error in result.Errors)
-        {
-            string[] newDescriptions;
-
-            if (errorDictionary.TryGetValue(error.Code, out string[]? descriptions))
-            {
-                newDescriptions = new string[descriptions.Length + 1];
-                Array.Copy(descriptions, newDescriptions, descriptions.Length);
-                newDescriptions[descriptions.Length] = error.Description;
-            }
-            else
-            {
-                newDescriptions = [error.Description];
-            }
-
-            errorDictionary[error.Code] = newDescriptions;
-        }
+        Dictionary<string, string[]> errorDictionary = result
+            .Errors.GroupBy(e => e.Code)
+            .ToDictionary(g => g.Key, g => g.Select(e => e.Description).ToArray());
 
         return TypedResults.ValidationProblem(errorDictionary);
     }
--- src/PlaceAPi.Identity/Authenticate/Endpoints/ResendConfirmationEmail.cs
+++ src/PlaceAPi.Identity/Authenticate/Endpoints/ResendConfirmationEmail.cs
@@ -7,12 +7,10 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.Data;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Routing;
 using Microsoft.AspNetCore.WebUtilities;
 using Microsoft.Extensions.DependencyInjection;
 
-// ReSharper disable once CheckNamespace
 public static partial class AuthenticationEndpointsExtensions
 {
     public static IEndpointRouteBuilder MapResendConfirmationEmailEndpoint<TUser>(
@@ -25,20 +23,19 @@ public static partial class AuthenticationEndpointsExtensions
         >();
         LinkGenerator linkGenerator = group.ServiceProvider.GetRequiredService<LinkGenerator>();
 
-        group.MapPost(
-            "/resendConfirmationEmail",
-            async Task<Ok> (
-                [FromBody] ResendConfirmationEmailRequest resendRequest,
-                HttpContext context,
-                [FromServices] IServiceProvider sp
-            ) =>
-            {
-                UserManager<TUser> userManager = sp.GetRequiredService<UserManager<TUser>>();
-                if (await userManager.FindByEmailAsync(resendRequest.Email) is not { } user)
-                {
-                    return TypedResults.Ok();
-                }
+        group.MapPost("/resendConfirmationEmail", ResendSendConfirmationEmailHandler);
 
+        return group;
+
+        async Task<Ok> ResendSendConfirmationEmailHandler(
+            [FromBody] ResendConfirmationEmailRequest resendRequest,
+            HttpContext context,
+            [FromServices] UserManager<TUser> userManager
+        )
+        {
+            TUser? user = await userManager.FindByEmailAsync(resendRequest.Email);
+            if (user != null)
+            {
                 await SendConfirmationEmailAsync(
                     emailSender,
                     linkGenerator,
@@ -47,14 +44,12 @@ public static partial class AuthenticationEndpointsExtensions
                     context,
                     resendRequest.Email
                 );
-                return TypedResults.Ok();
             }
-        );
-
-        return group;
+            return TypedResults.Ok();
+        }
     }
 
-    static async Task SendConfirmationEmailAsync<TUser>(
+    private static async Task SendConfirmationEmailAsync<TUser>(
         IEmailSender<TUser> emailSender,
         LinkGenerator linkGenerator,
         TUser user,
@@ -65,34 +60,83 @@ public static partial class AuthenticationEndpointsExtensions
     )
         where TUser : class
     {
-        if (confirmEmailEndpointName is null)
-        {
-            throw new NotSupportedException("No email confirmation endpoint was registered!");
-        }
+        EnsureConfirmEmailEndpointNameIsSet();
 
-        string code = isChange
-            ? await userManager.GenerateChangeEmailTokenAsync(user, email)
-            : await userManager.GenerateEmailConfirmationTokenAsync(user);
-        code = WebEncoders.Base64UrlEncode(Encoding.UTF8.GetBytes(code));
-
-        string userId = await userManager.GetUserIdAsync(user);
-        RouteValueDictionary routeValues = new() { ["userId"] = userId, ["code"] = code };
-
-        if (isChange)
-        {
-            routeValues.Add("changedEmail", email);
-        }
-
-        string confirmEmailUrl =
-            linkGenerator.GetUriByName(context, confirmEmailEndpointName, routeValues)
-            ?? throw new NotSupportedException(
-                $"Could not find endpoint named '{confirmEmailEndpointName}'."
-            );
+        (string code, string userId) = await GenerateConfirmationCodeAsync(
+            userManager,
+            user,
+            email,
+            isChange
+        );
+        RouteValueDictionary routeValues = CreateRouteValues(userId, code, email, isChange);
+        string confirmEmailUrl = GenerateConfirmationUrl(linkGenerator, context, routeValues);
 
         await emailSender.SendConfirmationLinkAsync(
             user,
             email,
             HtmlEncoder.Default.Encode(confirmEmailUrl)
         );
+    }
+
+    private static void EnsureConfirmEmailEndpointNameIsSet()
+    {
+        if (string.IsNullOrEmpty(confirmEmailEndpointName))
+        {
+            throw new InvalidOperationException("No email confirmation endpoint was registered.");
+        }
+    }
+
+    private static async Task<(string code, string userId)> GenerateConfirmationCodeAsync<TUser>(
+        UserManager<TUser> userManager,
+        TUser user,
+        string email,
+        bool isChange
+    )
+        where TUser : class
+    {
+        string code = isChange
+            ? await userManager.GenerateChangeEmailTokenAsync(user, email)
+            : await userManager.GenerateEmailConfirmationTokenAsync(user);
+
+        code = WebEncoders.Base64UrlEncode(Encoding.UTF8.GetBytes(code));
+        string userId = await userManager.GetUserIdAsync(user);
+
+        return (code, userId);
+    }
+
+    private static RouteValueDictionary CreateRouteValues(
+        string userId,
+        string code,
+        string email,
+        bool isChange
+    )
+    {
+        RouteValueDictionary routeValues = new RouteValueDictionary
+        {
+            ["userId"] = userId,
+            ["code"] = code,
+        };
+        if (isChange)
+        {
+            routeValues["changedEmail"] = email;
+        }
+        return routeValues;
+    }
+
+    private static string GenerateConfirmationUrl(
+        LinkGenerator linkGenerator,
+        HttpContext context,
+        RouteValueDictionary routeValues
+    )
+    {
+        if (confirmEmailEndpointName == null)
+        {
+            throw new InvalidOperationException("Email confirmation endpoint name is not set.");
+        }
+
+        return linkGenerator.GetUriByName(context, confirmEmailEndpointName, routeValues)
+            ?? throw new InvalidOperationException(
+                $"Could not generate URI for endpoint named '{confirmEmailEndpointName}'."
+            );
     }
 }
