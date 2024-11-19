286dc015bc4459375c419c5de6ba84755bba5c7c
GenjiruSUchiwa <genjiru23@gmail.com>
2024-11-19 16:01:59 +0100

    ci: update github workflow for ci

--- tests/PlaceApi.Identity.Tests.Integration/Endpoints/ConfirmEmail/V1/ConfirmEmailEndpointIntegrationTests.cs
+++ tests/PlaceApi.Identity.Tests.Integration/Endpoints/ConfirmEmail/V1/ConfirmEmailEndpointIntegrationTests.cs
@@ -12,7 +12,6 @@ using PlaceApi.Identity.Tests.Integration.Common;
 namespace PlaceApi.Identity.Tests.Integration.Endpoints.ConfirmEmail.V1;
 
 [Trait("Integration", "Identity")]
-[Collection("Sequential")]
 public class ConfirmEmailEndpointIntegrationTests(IdentityWebAppFactory factory)
     : IntegrationTestBase(factory)
 {
