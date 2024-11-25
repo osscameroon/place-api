0538777deb5e5f8cb155a9e8f74ca330fc78d5bc
GenjiruSUchiwa <60622115+GenjiruSUchiwa@users.noreply.github.com>
2024-11-25 05:52:46 +0100

    ci(deployment): Add deployment workflow and configuration files (#71)

---
+++ src/Place.API/HeathCheckExtensions.cs
@@ -0,0 +1,63 @@
+using System;
+using System.Linq;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Place.API;
+
+public static class HealthCheckExtensions
+{
+    public static IServiceCollection AddCustomHealthChecks(
+        this IServiceCollection services,
+        IConfiguration configuration
+    )
+    {
+        services
+            .AddHealthChecks()
+            .AddNpgSql(
+                name: "profile-db",
+                connectionString: configuration.GetConnectionString("Account")!,
+                tags: new[] { "db", "profile" }
+            )
+            .AddNpgSql(
+                name: "identity-db",
+                connectionString: configuration.GetConnectionString("Identity")!,
+                tags: new[] { "db", "identity" }
+            )
+            .AddUrlGroup(new Uri("http://seq:5341"), name: "seq", tags: new[] { "logging" });
+
+        return services;
+    }
+
+    public static IApplicationBuilder UseCustomHealthChecks(this IApplicationBuilder app)
+    {
+        app.UseHealthChecks(
+            "/health",
+            new HealthCheckOptions
+            {
+                ResponseWriter = async (context, report) =>
+                {
+                    context.Response.ContentType = "application/json";
+                    string result = System.Text.Json.JsonSerializer.Serialize(
+                        new
+                        {
+                            status = report.Status.ToString(),
+                            checks = report.Entries.Select(e => new
+                            {
+                                name = e.Key,
+                                status = e.Value.Status.ToString(),
+                                tags = e.Value.Tags,
+                            }),
+                        }
+                    );
+                    await context.Response.WriteAsync(result);
+                },
+            }
+        );
+
+        return app;
+    }
+}
